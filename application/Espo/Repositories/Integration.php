a5860d0c7471fb93e75ef2104721cfd905c6cf4d
abraham ghaemi <ig.ghaemi@gmail.com>
2019-03-15 03:06:19 +0330

    fix must be compatible in Integration

--- application/Espo/Repositories/Integration.php
+++ application/Espo/Repositories/Integration.php
@@ -33,13 +33,5 @@ use Espo\ORM\Entity;
 
 class Integration extends \Espo\Core\ORM\Repositories\RDB
 {
-    public function get($id = null) : ?Entity
-    {
-        $entity = parent::get($id);
-        if (empty($entity) && !empty($id)) {
-            $entity = $this->get();
-            $entity->id = $id;
-        }
-        return $entity;
-    }
+
 }
