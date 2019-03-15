783d8a3f7c11bf2fc0274d5ad12566d4c66a9c3b
abraham ghaemi <ig.ghaemi@gmail.com>
2019-03-16 01:45:53 +0330

    check rtl lang

--- client/src/views/preferences/edit.js
+++ client/src/views/preferences/edit.js
@@ -35,6 +35,10 @@ Espo.define('views/preferences/edit', 'views/edit', function (Dep) {
         setup: function () {
             Dep.prototype.setup.call(this);
             this.userName = this.model.get('name');
+            // snapp
+            Espo.Utils.setRtlStatus(this.model.get('language'));
+            // Espo.Utils.isRtl() ? Espo.Utils.addMetaLink() : '';
+            // end snapp
         },
 
         getHeader: function () {
