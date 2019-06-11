6f0ff4226047507ad7173e4d7b957f92cd22552b
ibrahem ghaemi <irvato@icloud.com>
2019-06-11 10:51:23 +0430

    add new music

--- custom/Espo/Custom/Controllers/Track.php
+++ custom/Espo/Custom/Controllers/Track.php
@@ -53,4 +53,36 @@ class Track extends \Espo\Core\Templates\Controllers\Base
 
         throw new Error();
     }
+
+    public function actionNew($params, $data, $request)
+    {
+        if (!$this->getAcl()->check($this->name, 'read')) {
+            throw new Forbidden();
+        }
+
+        $params = [];
+        $this->fetchListParamsFromRequest($params, $request, $data);
+
+        $maxSizeLimit = $this->getConfig()->get('recordListMaxSizeLimit', self::MAX_SIZE_LIMIT);
+        if (empty($params['maxSize'])) {
+            $params['maxSize'] = $maxSizeLimit;
+        }
+        if (!empty($params['maxSize']) && $params['maxSize'] > $maxSizeLimit) {
+            throw new Forbidden("Max size should should not exceed " . $maxSizeLimit . ". Use offset and limit.");
+        }
+
+        $result = $this->getRecordService()->find($params);
+
+        if (is_array($result)) {
+            return [
+                'total' => $result['total'],
+                'list' => isset($result['collection']) ? $result['collection']->getValueMapList() : $result['list']
+            ];
+        }
+
+        return [
+            'total' => $result->total,
+            'list' => isset($result->collection) ? $result->collection->getValueMapList() : $result->list
+        ];
+    }
 }
