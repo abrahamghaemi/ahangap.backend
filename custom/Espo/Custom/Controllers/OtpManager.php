b421109253d2724071380456060efcb3d6f91eca
ibrahem ghaemi <irvato@icloud.com>
2019-06-24 13:00:12 +0430

    implement otp sender

--- custom/Espo/Custom/Controllers/OtpManager.php
+++ custom/Espo/Custom/Controllers/OtpManager.php
@@ -4,4 +4,39 @@ namespace Espo\Custom\Controllers;
 
 class OtpManager extends \Espo\Core\Templates\Controllers\Base
 {
+    public function actionSend($params, $data, $request)
+    {
+        if (!is_object($data)) throw new BadRequest();
+
+        if (!$request->isPost()) {
+            throw new BadRequest();
+        }
+
+        if (!$this->getAcl()->check($this->name, 'create')) {
+            throw new Forbidden();
+        }
+
+        $service = $this->getRecordService();
+
+        $data = $this->normlizationDate($data);
+        $code = mt_rand(0000, 9999);
+        $data['name'] = $code;
+
+        if ($entity = $service->create($data)) {
+            $this->sendOtp($data);
+            return $entity->getValueMap();
+        }
+
+        throw new Error();
+    }
+
+    public function sendOtp($data)
+    {
+
+        $sender = "1000596446";
+        $message = "snapycloud verify code: " . $data['code'];
+        $api = new\Kavenegar\KavenegarApi("5435724C55454870633569354861705778766E303158543542583779356D7A6A");
+        $api->Send( $sender,$data['phone'], $message);
+        return true;
+    }
 }
