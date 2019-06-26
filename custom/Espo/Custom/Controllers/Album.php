ab4bcf15131192eb4ea3dd1781514abbdadbc1ef
ibrahem ghaemi <irvato@icloud.com>
2019-06-26 15:13:03 +0430

    add track list func

--- custom/Espo/Custom/Controllers/Album.php
+++ custom/Espo/Custom/Controllers/Album.php
@@ -23,4 +23,33 @@ class Album extends \Espo\Core\Templates\Controllers\Base
 
         return $this->getListOfTrack($params);
     }
+
+    private function getListOfTrack($params)
+    {
+        if (!$this->getAcl()->check($this->name, 'read')) {
+            throw new Forbidden();
+        }
+
+        $maxSizeLimit = $this->getConfig()->get('recordListMaxSizeLimit', self::MAX_SIZE_LIMIT);
+        if (empty($params['maxSize'])) {
+            $params['maxSize'] = $maxSizeLimit;
+        }
+        if (!empty($params['maxSize']) && $params['maxSize'] > $maxSizeLimit) {
+            throw new Forbidden("Max size should should not exceed " . $maxSizeLimit . ". Use offset and limit.");
+        }
+
+        $result = $this->getRecordService()->find($params);
+
+        if (is_array($result)) {
+            return [
+                'total' => $result['total'],
+                'list' => isset($result['collection']) ? $result['collection']->getValueMapList() : $result['list']
+            ];
+        }
+
+        return [
+            'total' => $result->total,
+            'list' => isset($result->collection) ? $result->collection->getValueMapList() : $result->list
+        ];
+    }
 }
