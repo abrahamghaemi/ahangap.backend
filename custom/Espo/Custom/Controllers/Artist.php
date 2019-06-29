248defa53505d4208db199792194015d354cd2f0
abraham ghaemi <=>
2019-06-29 23:24:46 +0430

    add route  popular artist

--- custom/Espo/Custom/Controllers/Artist.php
+++ custom/Espo/Custom/Controllers/Artist.php
@@ -2,6 +2,46 @@
 
 namespace Espo\Custom\Controllers;
 
-class Artist extends \Espo\Core\Templates\Controllers\Base
+use Espo\Core\Templates\Controllers\Base as Controller;
+
+class Artist extends Controller
 {
+    public function actionPopular($params, $data, $request)
+    {
+        $params = [];
+        $this->fetchListParamsFromRequest($params, $request, $data);
+        $params['orderBy'] = "like";
+        $params['order'] = "desc";
+
+        return $this->getListOfTrack($params);
+    }
+
+    public function getListOfTrack($params)
+    {
+        if (!$this->getAcl()->check($this->name, 'read')) {
+            throw new Forbidden();
+        }
+
+        $maxSizeLimit = $this->getConfig()->get('recordListMaxSizeLimit', self::MAX_SIZE_LIMIT);
+        if (empty($params['maxSize'])) {
+            $params['maxSize'] = $maxSizeLimit;
+        }
+        if (!empty($params['maxSize']) && $params['maxSize'] > $maxSizeLimit) {
+            throw new Forbidden("Max size should should not exceed " . $maxSizeLimit . ". Use offset and limit.");
+        }
+
+        $result = $this->getRecordService()->find($params);
+
+        if (is_array($result)) {
+            return [
+                'total' => $result['total'],
+                'list' => isset($result['collection']) ? $result['collection']->getValueMapList() : $result['list']
+            ];
+        }
+
+        return [
+            'total' => $result->total,
+            'list' => isset($result->collection) ? $result->collection->getValueMapList() : $result->list
+        ];
+    }
 }
