d413708023efff0197cb27e8adc3bd8605c862cc
abrahamfast <ig.ghaemi@gmail.com>
2020-04-11 15:23:16 -0400

    update playlists

--- custom/Espo/Custom/Controllers/Playlist.php
+++ custom/Espo/Custom/Controllers/Playlist.php
@@ -4,6 +4,45 @@ namespace Espo\Custom\Controllers;
 
 class Playlist extends \Espo\Core\Templates\Controllers\Base
 {
+
+    public function actionList($params, $data, $request)
+    {
+ if (!$this->getAcl()->check($this->name, 'read')) {
+            throw new Forbidden();
+        }
+
+        $params = [];
+        $this->fetchListParamsFromRequest($params, $request, $data);
+
+        $maxSizeLimit = $this->getConfig()->get('recordListMaxSizeLimit', self::MAX_SIZE_LIMIT);
+        if (empty($params['maxSize'])) {
+            $params['maxSize'] = $maxSizeLimit;
+        }
+        if (!empty($params['maxSize']) && $params['maxSize'] > $maxSizeLimit) {
+            throw new Forbidden("Max size should should not exceed " . $maxSizeLimit . ". Use offset and limit.");
+        }
+
+        if($_SERVER['HTTP_CLIENTID']) {
+
+            $where = [['type'=>'isFalse', 'attribute'=>'private']];
+            $params['where'] = $where;
+        }
+
+
+        $result = $this->getRecordService()->find($params);
+
+        if (is_array($result)) {
+            return [
+                'total' => $result['total'],
+                'list' => isset($result['collection']) ? $result['collection']->getValueMapList() : $result['list']
+            ];
+        }
+
+        return [
+            'total' => $result->total,
+            'list' => isset($result->collection) ? $result->collection->getValueMapList() : $result->list
+        ];
+    }
     public function actionPublic($params, $data, $request)
     {
 
